976d1b3b773cdd6dd36969b54a097258be6a9348
Rael Max <contato@raelmax.com>
2012-04-23 13:29:38 -0300

    js de portifolio

--- js/script.js
+++ js/script.js
@@ -5,4 +5,14 @@
 $(document).ready(function(){
     $('#menu').onePageNav({changeHash: true});
     $('#page').find('section').css({height: $(window).height()});
+    $('#projects-list').find('li').each(function(){
+        $this = $(this);
+        $this.find('div').hide();
+        console.log('hah');
+        $this.hover(function(){
+            $(this).find('div').stop(true, true).fadeIn('slow');
+        }, function(){
+            $(this).find('div').stop(true, true).fadeOut('slow');
+        });
+    });
 });
